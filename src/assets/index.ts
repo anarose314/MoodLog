0051c58279f25b61f6f11835ac4b1019ca1ef622
anarose314 <dpgp706@gmail.com>
2026-07-29 17:38:45 +0900

    🔧 Chore: 프로젝트 에셋 이미지 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as emotion1 } from './emotion1.png';
+export { default as emotion2 } from './emotion2.png';
+export { default as emotion3 } from './emotion3.png';
+export { default as emotion4 } from './emotion4.png';
+export { default as emotion5 } from './emotion5.png';
