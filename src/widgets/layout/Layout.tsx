97cf05918957f5015950bebeb13f6ab0515c89ba
anarose314 <66053969+anarose314@users.noreply.github.com>
2026-07-29 21:01:35 +0900

    ✨ Feat: 공통 헤더 컴포넌트 작성 (#5)

--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from '@/widgets/header';
 
 export default function Layout() {
   return (
-    <main className="shadow-box mx-auto min-h-screen max-w-150 bg-white">
+    <main className="shadow-box mx-auto min-h-screen max-w-150 bg-white px-5">
+      <Header title={'2026.07.29'} />
       <Outlet />
     </main>
   );
