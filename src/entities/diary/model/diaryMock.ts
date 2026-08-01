2c4092b6771c95d56c231a5637c8f230a2b77b6c
anarose314 <dpgp706@gmail.com>
2026-08-01 18:25:15 +0900

    ✨ Feat: entities/diary 목업 데이터 및 타입 추가

---
+++ src/entities/diary/model/diaryMock.ts
@@ -0,0 +1,16 @@
+import type { Diary } from './types';
+
+export const MOCK_DATA: Diary[] = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: '1번 일기 내용',
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: '2번 일기 내용',
+  },
+];
