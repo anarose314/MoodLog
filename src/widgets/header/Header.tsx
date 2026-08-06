877f16fc301f1e9f831a56e39f701aad388961ca
김예지 <66053969+anarose314@users.noreply.github.com>
2026-08-06 23:09:56 +0900

    ✨ Feat: 공통 헤더 날짜 연동 및 날짜 넘기는 기능 추가 (#11)

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,21 +1,28 @@
+import { useState } from 'react';
 import Button from '@/shared/ui/Button';
 
-interface HeaderProps {
-  title: string;
-}
+export default function Header() {
+  const [pivotDate, setPivotDate] = useState(new Date());
+  const title = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`;
 
-export default function Header({ title }: HeaderProps) {
+  const handleDecreaseMonth = () => {
+    setPivotDate((prev) => new Date(prev.getFullYear(), prev.getMonth() - 1));
+  };
+  const handleIncreaseMonth = () => {
+    setPivotDate((prev) => new Date(prev.getFullYear(), prev.getMonth() + 1));
+  };
+
   return (
     <header className="mb-5 border-b border-b-gray-300 py-5">
       <ul className="flex w-full items-center justify-between">
         <li>
-          <Button>&lt;</Button>
+          <Button onClick={handleDecreaseMonth}>&lt;</Button>
         </li>
         <li>
           <p>{title}</p>
         </li>
         <li>
-          <Button>&gt;</Button>
+          <Button onClick={handleIncreaseMonth}>&gt;</Button>
         </li>
       </ul>
     </header>
--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -4,7 +4,7 @@ import { Header } from '@/widgets/header';
 export default function Layout() {
   return (
     <main className="shadow-box mx-auto min-h-screen max-w-150 bg-white px-5">
-      <Header title={'2026.07.29'} />
+      <Header />
       <Outlet />
     </main>
   );
