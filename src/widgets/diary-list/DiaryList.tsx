d6f5763270d5682ffc96a1e48b5d45f950058520
anarose314 <dpgp706@gmail.com>
2026-08-06 22:53:54 +0900

    🐛 Fix: select 태그에 name, id, aria-label 지정 및 option value의 오타 수정

--- src/widgets/diary-list/DiaryList.tsx
+++ src/widgets/diary-list/DiaryList.tsx
@@ -5,9 +5,14 @@ export default function DiaryList() {
   return (
     <>
       <div className="flex gap-3">
-        <select className="cursor-pointer rounded-[5px] bg-gray-100 px-5 py-2.5">
+        <select
+          name="sort"
+          id="sort"
+          aria-label="일기 정렬"
+          className="cursor-pointer rounded-[5px] bg-gray-100 px-5 py-2.5"
+        >
           <option value="latest">최신순</option>
-          <option value="oldset">오래된순</option>
+          <option value="oldest">오래된순</option>
         </select>
         <Button variant="POSITIVE" className="grow">
           새 일기 쓰기
