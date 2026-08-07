57ed2a206660f77ba2a9cc764e23abe64e7388e7
김예지 <66053969+anarose314@users.noreply.github.com>
2026-08-07 23:12:26 +0900

    ✨ Feat: 일기 리스트 최신순/오래된순 정렬 추가 (#14)

--- src/widgets/diary-list/DiaryList.tsx
+++ src/widgets/diary-list/DiaryList.tsx
@@ -1,3 +1,4 @@
+import { type ChangeEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getMonthlyData } from '@/entities/diary';
 import {
@@ -8,10 +9,27 @@ import { DiaryItem } from '@/features/diary-item';
 import Button from '@/shared/ui/Button';
 
 export default function DiaryList() {
+  const [sort, setSort] = useState('latest');
+
   const { pivotDate } = useDateState();
   const data = useDiaryState();
   const filteredData = getMonthlyData(pivotDate, data);
+  const sortedData = filteredData.toSorted((a, b) => {
+    const aCreatedDate = Number(a.createdDate);
+    const bCreatedDate = Number(b.createdDate);
+
+    if (sort === 'latest') {
+      return bCreatedDate - aCreatedDate;
+    } else {
+      return aCreatedDate - bCreatedDate;
+    }
+  });
+
   const nav = useNavigate();
+
+  const handleSortChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSort(e.target.value);
+  };
 
   return (
     <>
@@ -21,6 +39,7 @@ export default function DiaryList() {
           id="sort"
           aria-label="일기 정렬"
           className="cursor-pointer rounded-[5px] bg-gray-100 px-5 py-2.5"
+          onChange={handleSortChange}
         >
           <option value="latest">최신순</option>
           <option value="oldest">오래된순</option>
@@ -30,7 +49,7 @@ export default function DiaryList() {
         </Button>
       </div>
       <ul>
-        {filteredData.map((item) => (
+        {sortedData.map((item) => (
           <li key={item.id}>
             <DiaryItem item={item} />
           </li>
