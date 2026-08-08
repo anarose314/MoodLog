b1cd88dd39f1168add8e0ff2f7c4536e44330dd3
anarose314 <dpgp706@gmail.com>
2026-08-08 22:51:32 +0900

    🐛 Fix: 불필요한 프래그먼트 삭제

--- src/widgets/new-header/NewHeader.tsx
+++ src/widgets/new-header/NewHeader.tsx
@@ -3,11 +3,6 @@ import Header from '@/shared/ui/Header';
 
 export default function NewHeader() {
   return (
-    <>
-      <Header
-        title="새 일기 쓰기"
-        leftChild={<Button>&lt; 뒤로 가기</Button>}
-      />
-    </>
+    <Header title="새 일기 쓰기" leftChild={<Button>&lt; 뒤로 가기</Button>} />
   );
 }
