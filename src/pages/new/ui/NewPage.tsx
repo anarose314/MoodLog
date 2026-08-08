6c1909b5a33ff415d7c6eca721fc1a66ef171f78
anarose314 <dpgp706@gmail.com>
2026-08-08 22:46:49 +0900

    ✨ Feat: new 페이지에 헤더 추가

--- src/pages/new/ui/NewPage.tsx
+++ src/pages/new/ui/NewPage.tsx
@@ -1,6 +1,9 @@
+import { NewHeader } from '@/widgets/new-header';
+
 export default function NewPage() {
   return (
     <>
+      <NewHeader />
       <div className="">New</div>
     </>
   );
---
+++ src/widgets/new-header/NewHeader.tsx
@@ -0,0 +1,13 @@
+import Button from '@/shared/ui/Button';
+import Header from '@/shared/ui/Header';
+
+export default function NewHeader() {
+  return (
+    <>
+      <Header
+        title="새 일기 쓰기"
+        leftChild={<Button>&lt; 뒤로 가기</Button>}
+      />
+    </>
+  );
+}
