8cc485518f4234721e0d5138a72102a6f2780fc0
anarose314 <dpgp706@gmail.com>
2026-08-10 22:51:57 +0900

    🐛 Fix: 공통 Button의 border-radius 값 수정

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const ButtonVariants = cva(
-  'typo-2lg-regular cursor-pointer rounded-[5px] border-0 px-5 py-2.5 text-nowrap disabled:cursor-not-allowed',
+  'typo-2lg-regular cursor-pointer rounded-md border-0 px-5 py-2.5 text-nowrap disabled:cursor-not-allowed',
   {
     variants: {
       variant: {
